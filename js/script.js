1dfc42e459b84ccdfcd547312659398cc8afa0e1
Karol Dabrowski <dabrowski.carl@gmail.com>
2018-07-13 01:35:04 +0100

    Add cards to index

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    //put cards into variables
+        //back
+    const backCard = document.getElementById('backCard');
+        //tens
+    const clubs10 = document.getElementById('10clubs');
+    const diamonds10 = document.getElementById('10diamonds');
+    const heats10 = document.getElementById('10hearts');
+    const spades10 = document.getElementById('10spades');
+        //twoes
+    
+});
